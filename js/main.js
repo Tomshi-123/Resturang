fb052bb176f7e0cbbc89e3f65471927c10431cbd
alexander <alexander.t.simonsen@gmail.com>
2025-03-21 11:38:19 +0100

    manybilderPush

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import "./menu-toggle.js";
+import "./modal.js";
+import "./fadein.js";
+import "./shopping.js";
+
+// Om du vill skriva något direkt när DOM är laddad:
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Alla skript är nu laddade!");
+});
